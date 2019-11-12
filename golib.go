3ebb12f973e4933a310a363ae152fca39e0de23d
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-11-12 23:04:12 +0500

    .

---
+++ golib.go
@@ -0,0 +1,11 @@
+package golib
+
+import (
+	"fmt"
+)
+
+// ImportCheck is an exported function.
+// Which can be used as golib.ImportCheck()
+func ImportCheck() {
+	fmt.Println("golib imported")
+}
